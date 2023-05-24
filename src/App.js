8b13536853ef1c5af1e4129517d6b9b5cbe2d3b8
ishikakodwani! <ishikakodwani15@gmail.com>
2023-05-24 12:10:03 +0530

    update 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import { Routes ,Route, Link, BrowserRouter} from 'react-router-dom';
-import { Home } from './Home';
-import { About } from './About';
-import { NotFound } from './Found';
-import Logo from "./logo.svg";
+import './style.css'
+import { Routes ,Route,BrowserRouter} from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import { NotFound } from './Pages/Found';
+import { Navbar } from './Pages/Navbar';
+import {Login} from './Pages/Login';
+
 function App() {
   return (<>
-  <img src={Logo} alt="logo" style={{height:50}}/>
+  
   <BrowserRouter>
-  <div className="navbarstyle"> 
-  <Link to="/" style={{marginLeft:5,color:'black',textDecoration: 'none',columnGap:5}}>Home</Link>
-  <Link to="/about" style={{marginLeft:10 ,color:'black',textDecoration: 'none',columnGap:5}} >About</Link>
-  <Link to="/contact" style={{marginLeft:10 ,color:'black',textDecoration: 'none',columnGap:5 }} >Contact</Link></div>
+  <Navbar/>
   <Routes>
     <Route path="/" element={<Home/>}>
     </Route> 
      <Route path="/about" element={<About/>}>
     </Route>  
-    <Route path="*" element={ < NotFound/>}>
+     <Route path="/login" element={<Login/>}>
+    </Route>  
+    <Route path="*" element={<NotFound/>}>
     </Route> 
   </Routes>
     </BrowserRouter>
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom';
+import Logo from '../logo.svg';
+export const Navbar=()=>{ return(<>
+
+<div className="navbarstyle">
+ <img src={Logo} alt="logo" style={{height:20}}/>
+  <Link to="/" style={{marginLeft:5,color:'black',textDecoration: 'none',columnGap:5}}>Home</Link>
+  <Link to="/about" style={{marginLeft:10 ,color:'black',textDecoration: 'none',columnGap:5}} >About</Link>
+  <Link to="/contact" style={{marginLeft:10 ,color:'black',textDecoration: 'none',columnGap:5 }} >Contact</Link>
+  <Link to="/login" style={{marginLeft:10 ,color:'black',textDecoration: 'none',columnGap:5 }} >Login</Link>
+  </div></>)};
